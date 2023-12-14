582925b6256fc2d5deb91812daeaba0e91292f3a
Behnam_mehdipoor <149398325+Behnammmmmmmmm@users.noreply.github.com>
2023-12-14 12:47:59 +0330

    Add files via upload

--- os-project-1.py
+++ os-project-1.py
@@ -20,6 +20,8 @@ dict_save={}
 y_author=[]
 y_image=[]
 y_category=[]
+y_title=[]
+y_tag=[]
 
 if re.findall('https://isorepublic.com/photo/.+/"',soup.prettify()):
     list_save.append(re.findall('https://isorepublic.com/photo/.+/"',soup.prettify()))
@@ -31,29 +33,35 @@ list_save=list(list_save_del_reaprat)
 
 for item in range(len(list_save)):
     request_image_page=scrapper.get(list_save[item])
-    with open('behnam00.csv','wb') as q:
-        q.write(request_image_page.content)
+    
+    use=request_image_page.content
             
-    soup1 = BeautifulSoup (open("behnam00.csv"), features="html.parser")
-                
+    soup1 = BeautifulSoup (use, features="html.parser")
     if re.findall('href="https://isorepublic.com/wp-content/uploads.+.jpg',soup1.prettify()):
         y_image.append(re.findall('href="https://isorepublic.com/wp-content/uploads.+.jpg',soup1.prettify()))
         y_author.append(re.findall('https://isorepublic.com/media-author.+" ',soup1.prettify()))
         y_category.append(re.findall('href="https://isorepublic.com/media-category.+/"',soup1.prettify()))
+        mat=re.search('<h1>.*</h1>',soup1.prettify(),re.DOTALL)
+        y_title.append(mat.group(0))
+        mat2=re.search('<ul class="keyword-tags".+</section>',soup1.prettify(),re.DOTALL)
+        y_tag.append(mat2.group(0))
         
         dict_save[y_author[item][0]]=y_image[item][0]
 
+
 #-------------------------in ghesmat bad moshakhasat ti file save mikona
-    
-header=['category','author','tag','url']
+
+flag=1    
+header=['category','author','title','url','tag']
 for item1 in range(len(list_save)):
-    data=[y_category[item1][0][45:-3],y_author[item1][0][37:-2],y_author[item1][0][37:-2],y_image[item1][0][6:]]
+    data=[y_category[item1][0][45:-3],y_author[item1][0][37:-2], y_title[item1],y_image[item1][0][6:],y_tag[item1]]
     with open('inform.csv', 'a', encoding='UTF8') as f5:
         writer = csv.writer(f5)
-        writer.writerow(header)
+        if flag==1:
+            writer.writerow(header)
+            flag +=1
         writer.writerow(data)
-#print(y_category)
-# az inja be bad dige sakht poshe v download photo ast
+        
 
 list_check_not_reapeat_folder=[]
 for d1,d2 in dict_save.items():
@@ -64,4 +72,4 @@ for d1,d2 in dict_save.items():
     with open(f"{str(d1[37:-2])}/new.jpg",'wb') as save5:
         save5.write(request3.content)
     list_check_not_reapeat_folder.append(str(d1[37:-2]))
-  
+
